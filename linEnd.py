a5760050a31c3d74aa719b6fb4d8c45e44adec03
Ida Dahl <ida.josephine.dahl@gmail.com>
2025-02-18 15:35:08 +0100

    adding naive end linearization

---
+++ linEnd.py
@@ -0,0 +1,11 @@
+
+def naive_start(inp: dict):
+    puts = dict()
+    gets = dict()
+    for i in inp:
+        timestamp = inp[i]
+        puts.update({i: timestamp.enq_end})
+        if timestamp.deq_end != None:
+            gets.update({i: timestamp.deq_end})
+
+    return (puts,gets)
