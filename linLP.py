d9433831e5c158525fb39b3f50b024ea10f68690
Paint19 <hanna@schaff.se>
2025-03-17 11:34:03 +0100

    WIP lp linearization. This solution is more general than the previous, but does not handle deq None or a potential window solution

--- linLP.py
+++ linLP.py
@@ -1,126 +1,104 @@
 import cvxpy as cp
 import numpy as np
 
+# Creates two arrays of cvxpy variable arrays, one for enqueue operations and one for dequeue operations
+# Each variable array corresponds a single enq/deq operation
+# Each variable corresponds to a potential order of the enq/deq operation
+# Only one variable per operation can be set to 1, the rest must be 0. This is regulated in the constraints in linear_programming().
+#  E = np.array([
+#    [x11, x12, x13, x14], # Indices corresponds to possible order positions for first operation
+#    [x21, x22, x23, x24],
+#    [x31, x32, x33, x34],
+#    [x41, x42, x43, x44]])
+# n x n matrices, where n is the number of values (i.e. number of enq operations). For both enq/deq matrices.
+# I.e. big ass matrices. That CVXPY probably will not like later when we run for large n.
+def get_variable_matrix(inp:dict, n):
+    E = []
+    D = []
+    for i, tuple in enumerate(inp.values()):
+        # TODO: Want to NOT APPEND when tuple[1] contains None. However, that will mess with the order in objective function. This current solution simply cannot handle deq None.            
+        E.append(cp.Variable(n, integer=True))
+        D.append(cp.Variable(n, integer=True))
+    return (E, D)
 
-# TODO: Consider dequeue None
-# TODO: Check that enqueue order is before dequeue order. CONSTRAINT!
+def get_objective_function(E, D, n):
+    order_vector = np.array(range(1, n+1))
+    potential_enq_order = np.array(E)
+    potential_deq_order = np.array(D)
+    return cp.Minimize(cp.sum(cp.abs(potential_enq_order @ order_vector - potential_deq_order @ order_vector)))
 
-def linear_programming():
+# Expects an array A with n rows and m columns, outputs an array A with m rows and n columns
+# Expects rows to be cvxpy-variables
+def transpose(A):
+    n = len(A)
+    m = A[0].size
+    transposed_A = [[None for _ in range(0,n)] for _ in range(0,m)] # n columns, m rows
+    # Iterate A and add values to transposed_A
+    for i in range(0, n):
+        for j in range(0, m):
+            transposed_A[j][i] = A[i][j]
+    return transposed_A
 
-    """E = np.array([
-    [1, 1, 1, 0], # Indices corresponds to possible order positions
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [0, 1, 1, 1]])
+def get_constraints(inp:dict, E, D, n):
+    # when creating constraints, set corresponding variable to 0 if it does not exist in order_list, 
+    # and do "sum rest of the variables = 1"
+    constraints = []
+    for i, tuple in enumerate(inp.values()):
+        constraints.append(E[i] >= 0) # Binary variables
+        constraints.append(E[i] <= 1)
+        constraints.append(D[i] >= 0)
+        constraints.append(D[i] <= 1)
 
-    D = np.array([
-    [1, 1, 1, 0],
-    [1, 1, 1, 0],
-    [1, 1, 1, 1],
-    [0, 0, 1, 1]])
+        enq_order_list = tuple[0] # Potential orderings for this particular value's enqueue
+        deq_order_list = tuple[1]
+
+        # If value does not have order j as a potential enq/deq order, set corresponding variable to 0 as constraint
+        for j in range(0, n): 
+            if(j not in enq_order_list): 
+                constraints.append(E[i][j] == 0)
+            if (j not in deq_order_list): 
+                constraints.append(D[i][j] == 0)
+
+        constraints.append(cp.sum(E[i]) == 1) # Each operation can only have a single order
+        constraints.append(cp.sum(D[i]) == 1) 
+
+    transposed_E = transpose(E)
+    transposed_D = transpose(D)
+
+    for i in range(0, n):
+        constraints.append(cp.sum(transposed_E[i]) == 1) # Each order can only be "occupied" by a single operation
+        constraints.append(cp.sum(transposed_D[i]) == 1)
     
-    enqOrd = [0, 1, 2, 3]
-    deqOrd = [0, 1, 2, 3]
+    return constraints
 
-    b = np.array([0,0,0,0,0,0,1,2])
-    c = np.array([2,2,3,2,3,3,3,3])"""
+def linear_programming(inp:dict):
+    n = len(inp)
+    (E, D) = get_variable_matrix(inp, n)
 
-    #x = cp.Variable(8, integer=True) # each x is 0 or 1 
-    x = cp.Variable(4*4*2, integer=True) # First 4*2 values are for enqueue and the rest for dequeue
-
-    #objective_function = (x[0] - x[1]) + (x[2] - x[3]) + (x[4] - x[5]) + (x[6] - x[7])
-    objective_function = ((x[0]*1 + x[1]*2 + x[2]*3 + x[3]*4) - (x[16]*1 + x[17]*2 + x[18]*3 + x[19]*4) + 
-                          (x[4]*1 + x[5]*2 + x[6]*3 + x[7]*4) - (x[20]*1 + x[21]*2 + x[22]*3 + x[23]*4) +
-                          (x[8]*1 + x[9]*2 + x[10]*3 + x[11]*4) - (x[24]*1 + x[25]*2 + x[26]*3 + x[27]*4) +
-                          (x[12]*1 + x[13]*2 + x[14]*3 + x[15]*4) - (x[28]*1 + x[29]*2 + x[30]*3 + x[31]*4))
-
-    constraints = [
-        x >= 0, x <= 1, #"binary" variables
-        x[3] == 0, x[12] == 0, x[19] == 0, x[23] == 0, x[28] == 0, x[29] == 0,   # All x values which are NOT potential orders
-        
-        # Each enqueue operation must have exactly one of the possible positions
-        x[0] + x[1] + x[2] == 1,
-        x[4] + x[5] + x[6] + x[7] == 1,
-        x[8] + x[9] + x[10] + x[11] == 1,
-        x[13] + x[14] + x[15] == 1,
-        # Only one of each enqueue operation can take each order position
-        x[0] + x[4] + x[8] == 1,
-        x[1] + x[5] + x[9] + x[13] == 1,
-        x[2] + x[6] + x[10] + x[14] == 1,
-        x[7] + x[11] + x[15] == 1,
-
-        # Each dequeue operation must have exactly one of the possible positions
-        x[16] + x[17] + x[18] == 1,
-        x[20] + x[21] + x[22] == 1,
-        x[24] + x[25] + x[26] + x[27] == 1,
-        x[30] + x[31] == 1,
-        # Only one of each dequeue operation can take each order position
-        x[16] + x[20] + x[24] == 1,
-        x[17] + x[21] + x[25] == 1,
-        x[18] + x[22] + x[26] + x[30] == 1,
-        x[27] + x[31] == 1
-    ]
-
-    prob=cp.Problem(cp.Minimize(objective_function), constraints=constraints)
+    problem=cp.Problem(get_objective_function(E, D, n), constraints=get_constraints(inp, E, D, n))
     
-    prob.solve()
+    problem.solve()
+    print("Value: ", problem.value)
+    for e in E:
+        print("Solution to e:", e.value)
+    for d in D:
+        print("Solution to d:", d.value)
 
-    print("Value: ", prob.value)
-    print("Solution to x: ", x.value)
 
-
-testdict = {
-        1: ({0,1,2}, {0,1,2}),
-        2: ({0,1,2,3},{0,1,2}),
-        3: ({0,1,2,3},{0,1,2,3}),
-        4: ({1,2,3},{2,3})
-        #5: ({},{None})
+testdict_no_rank_error = {                    # Har ingen rank error
+        1: ([0,1,2], [0,1,2]),
+        2: ([0,1,2,3],[0,1,2]),
+        3: ([0,1,2,3],[0,1,2,3]),
+        4: ([1,2,3],[2,3]),
+        #5: ([1,2],[None])
     }
 
-def hard_coded_example_with_rank_error():
-    x = cp.Variable(4*4*2, integer=True) # First 4*2 values are for enqueue and the rest for dequeue
-    #objective_function = (x[0] - x[1]) + (x[2] - x[3]) + (x[4] - x[5]) + (x[6] - x[7])
-    objective_function = (cp.abs((x[0]*1 + x[1]*2 + x[2]*3 + x[3]*4) - (x[16]*1 + x[17]*2 + x[18]*3 + x[19]*4)) + 
-                        cp.abs((x[4]*1 + x[5]*2 + x[6]*3 + x[7]*4) - (x[20]*1 + x[21]*2 + x[22]*3 + x[23]*4)) +
-                        cp.abs((x[8]*1 + x[9]*2 + x[10]*3 + x[11]*4) - (x[24]*1 + x[25]*2 + x[26]*3 + x[27]*4)) +
-                        cp.abs((x[12]*1 + x[13]*2 + x[14]*3 + x[15]*4) - (x[28]*1 + x[29]*2 + x[30]*3 + x[31]*4)))
-    constraints = [
-    x >= 0, x <= 1, #"binary" variables
-    x[2] == 0, x[3] == 0, x[6] == 0, x[7] == 0, 
-    x[8] == 0, x[9] == 0, x[12] == 0, x[13] == 0,
+testdict_has_rank_error = {                    # Har min rank error 4. Motsvarar dock ej value.
+        1: ([0,1], [2,3]),
+        2: ([0,1], [2,3]),
+        3: ([2,3],[0,1]),
+        4: ([2,3],[0,1]),
+        #5: ([0,1,2,3],[None])
+    }
 
-    x[15] == 0, x[16] == 0, x[19] == 0, x[20] == 0,
-    x[25] == 0, x[26] == 0, x[29] == 0, x[30] == 0,
-
-    # Each enqueue operation must have exactly one of the possible positions
-    x[0] + x[1] == 1,
-    x[4] + x[5] == 1,
-    x[10] + x[11] == 1,
-    x[14] + x[15] == 1,
-    # Only one of each enqueue operation can take each order position
-    x[0] + x[4] == 1,
-    x[1] + x[5] == 1,
-    x[10] + x[14] == 1,
-    x[11] + x[15] == 1,
-
-    # Each dequeue operation must have exactly one of the possible positions
-    x[17] + x[18] == 1,
-    x[21] + x[22] == 1,
-    x[23] + x[24] == 1,
-    x[27] + x[28] == 1,
-    # Only one of each dequeue operation can take each order position
-    x[17] + x[21] == 1,
-    x[18] + x[22] == 1,
-    x[23] + x[27] == 1,
-    x[24] + x[28] == 1
-    ]
-    prob=cp.Problem(cp.Minimize(objective_function), constraints=constraints)
-
-    prob.solve()
-
-    print("💖💖💖💖💖💖")
-    print("Value: ", prob.value)
-    print("Solution to x: ", x.value)
-
-#linear_programming(testdict)
-hard_coded_example_with_rank_error()
+linear_programming(testdict_no_rank_error)
