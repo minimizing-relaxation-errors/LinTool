165fd9d7db43719a4eb21c3c5458c705e22f8df4
Ida Dahl <ida.josephine.dahl@gmail.com>
2025-03-19 13:00:30 +0100

    removed moved timestamp class

--- linearizationTool.py
+++ linearizationTool.py
@@ -37,24 +37,6 @@ class Linearization(Enum):
     TryTwentyFive = auto()
 
 
-'''
-## Time stamp class, creating object containing 4 timestamps
-class Timestamp:
-    def __init__(self, enq_s, enq_e, deq_s, deq_e):
-        self.enq_start = enq_s
-        self.enq_end = enq_e
-        self.deq_start = deq_s
-        self.deq_end = deq_e
-    # we know the enq times before the deq times so we have a function to add them later
-    def update_deq(self, deq_s, deq_e):
-        self.deq_start = deq_s
-        self.deq_end = deq_e
-
-    def update_enq(self, enq_s, enq_e):
-        self.enq_end = enq_e
-        self.enq_start = enq_s
-
-'''
 def get_timestamps_from_file(filename):
     timestamps = dict() ## initiate dict for timestamps
     with open("timestamps/" + filename, newline='') as csvfile:
