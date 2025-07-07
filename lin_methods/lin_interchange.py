f3f4e798ab491745435251677a8fe99df9230c2d
Paint19 <hanna@schaff.se>
2025-07-07 10:54:51 +0200

    Runs enqueue and dequeue in same iteration for interchange

--- lin_methods/lin_interchange.py
+++ lin_methods/lin_interchange.py
@@ -13,7 +13,7 @@ class Op(Enum):  # TODO: Maybe move out
 
 # Global variables
 lin = {}                # dictionary of item:[enq, deq]
-overlapping_items = {}  # dictionary of item:[items overlapping in enqueue/dequeue interval] (depending on which init function is called)
+#overlapping_items = {}  # dictionary of item:[items overlapping in enqueue/dequeue interval] (depending on which init function is called)
 
 # Expects as input: existing_lin as a dictionary of item:[enq timestamp, deq timestamp],
 #                   start_end_timestamps as a dictionary of item:Timestamp,
@@ -23,6 +23,80 @@ overlapping_items = {}  # dictionary of item:[items overlapping in enqueue/deque
 def interchange(existing_lin, start_end_timestamps, nr_iterations, nr_swaps_stopping_criteria):
     original_last_timestamp = get_total_last_timestamp(start_end_timestamps)
     init_lin(start_end_timestamps, existing_lin, original_last_timestamp)
+
+    enq_overlapping = init_overlapping(Op.Enq, start_end_timestamps)
+    deq_overlapping = init_overlapping(Op.Deq, start_end_timestamps)
+    start_t = datetime.datetime.now() # Start timing first iteration
+    enq_pot_swaps = init_pot_swaps(start_end_timestamps, Op.Enq, enq_overlapping) # pot_swaps is item1:[(item2, re_imp)]
+    deq_pot_swaps = init_pot_swaps(start_end_timestamps, Op.Deq, deq_overlapping)
+    out_str = ""
+
+    initial_tot_rank_error = get_total_rank_error(original_last_timestamp)
+    print("Initial total rank error: ", initial_tot_rank_error, "\n")
+    out_str += "Initial total rank error: " + str(initial_tot_rank_error) + "\n"
+
+    count = 0
+    while count < nr_iterations: 
+        if count != 0: start_t = datetime.datetime.now()
+        # NOTE: May result in incorrect re_imps 
+        (enq_pot_swaps, nr_enq_swaps) = execute_and_update(enq_pot_swaps, start_end_timestamps, Op.Enq, enq_overlapping) 
+        (deq_pot_swaps, nr_deq_swaps) = execute_and_update(deq_pot_swaps, start_end_timestamps, Op.Deq, deq_overlapping)
+        count += 1
+        nr_swaps_this_iteration = nr_enq_swaps + nr_deq_swaps
+        end_t = datetime.datetime.now()
+        
+        # Prepare output prints:
+        tot_rank_error = get_total_rank_error(original_last_timestamp)
+        tmp_out_str =  ("ITERATION " + str(count) + ":\n" +
+                    "Nr enq swaps: " + str(nr_enq_swaps) + "\n" +
+                    "Nr deq swaps: " + str(nr_deq_swaps) + "\n" +
+                    "Time (min): " + get_minutes_str(start_t, end_t) + "\n" +
+                    "Total rank error after: " + str(tot_rank_error) + "\n")
+        print(tmp_out_str)
+        out_str += tmp_out_str
+        # Check if stopping critera achieved
+        if nr_swaps_stopping_criteria != None:
+            if nr_swaps_this_iteration <= nr_swaps_stopping_criteria: break
+
+    ###### OUTPUT DATA
+    puts = {}  
+    gets = {}
+    for (item,enq_deq) in lin.items():
+        puts[item] = enq_deq[0]
+        if enq_deq[1] <= original_last_timestamp:
+            gets[item] = enq_deq[1]             # Only save dequeue timestamp if they were not None originally
+
+    return (puts, gets, out_str)
+'''
+def interchange_partial(existing_lin, start_end_timestamps, nr_iterations, nr_swaps_stopping_criteria):
+    nr_items_total = len(existing_lin)
+    nr_items_to_process = 2000
+    step = 1000
+    count = 0
+    iteration_count = 0
+    original_last_timestamp = get_total_last_timestamp(start_end_timestamps)
+    init_lin(start_end_timestamps, existing_lin, original_last_timestamp)
+    
+    while iteration_count < nr_iterations:
+        start_ind = count
+        end_ind = count + nr_items_to_process
+        while count < end_ind:
+            
+            old_interchange_inner(existing_lin, start_end_timestamps, nr_iterations, nr_swaps_stopping_criteria, start_ind, end_ind)
+            count += step
+
+    ###### OUTPUT DATA
+    puts = {}  
+    gets = {}
+    for (item,enq_deq) in lin.items():
+        puts[item] = enq_deq[0]
+        if enq_deq[1] <= original_last_timestamp:
+            gets[item] = enq_deq[1]             # Only save dequeue timestamp if they were not None originally
+
+    return (puts, gets, out_str)
+
+def old_interchange_inner(existing_lin, start_end_timestamps, nr_iterations, nr_swaps_stopping_criteria, start_ind, end_ind):
+    
 
     ###### Optimize enqueue linearization points
     init_overlapping(Op.Enq, start_end_timestamps)
@@ -75,17 +149,7 @@ def interchange(existing_lin, start_end_timestamps, nr_iterations, nr_swaps_stop
         # Check if stopping criteria achieved
         if nr_swaps_stopping_criteria != None:
             if nr_swaps_this_iteration <= nr_swaps_stopping_criteria: break
-
-    ###### OUTPUT DATA
-    puts = {}  
-    gets = {}
-    for (item,enq_deq) in lin.items():
-        puts[item] = enq_deq[0]
-        if enq_deq[1] <= original_last_timestamp:
-            gets[item] = enq_deq[1]             # Only save dequeue timestamp if they were not None originally
-
-    return (puts, gets, out_str)
-
+'''
 def get_minutes_str(start, end):
     return str(round(((end-start) / datetime.timedelta(microseconds=1))/(60 * 1000000),4))
 
@@ -112,7 +176,7 @@ def init_lin(start_end_timestamps, existing_lin, original_last_timestamp):
 
 # For each item, stores all items for which intervals overlap (with regard to the operation op_type)
 def init_overlapping(op_type, start_end_timestamps):
-    global overlapping_items
+    overlapping_items = {}
     for item1, ts1 in start_end_timestamps.items():
         if op_type == Op.Enq:
             s1 = ts1.enq_start
@@ -135,6 +199,7 @@ def init_overlapping(op_type, start_end_timestamps):
                 items.append(item2)
         if items: # Don't add if items is empty
             overlapping_items[item1] = items
+    return overlapping_items
 
 # Computes total rank error for the linearization
 # NOTE: For some reason computes a slightly lower rank error than the method in compute_rank_error. Insignificant difference.
@@ -210,7 +275,7 @@ def swap_items(item1, item2, op_type):
 
 # Updates pot_swap[item_a] with the pot_swap[item_b] and swaps out its own occurence with item_b 
 # Returns updated pot_swaps
-def update_a_with_bs_list(item_a, item_b, pot_swaps, start_end_timestamps, op_type):
+def update_a_with_bs_list(item_a, item_b, pot_swaps, start_end_timestamps, op_type, overlapping_items):
     new_list = []
     for item in overlapping_items[item_b]:
         if item == item_a: item = item_b # Swap item_a with item_b in the list (since it will be assigned to item_a)
@@ -220,7 +285,7 @@ def update_a_with_bs_list(item_a, item_b, pot_swaps, start_end_timestamps, op_ty
     pot_swaps[item_a] = new_list
     return pot_swaps
 
-def update_other_a_lists_with_b(item_a, item_b, pot_swaps, start_end_timestamps, op_type):
+def update_other_a_lists_with_b(item_a, item_b, pot_swaps, start_end_timestamps, op_type, overlapping_items):
     pot_swaps_items = pot_swaps.keys()
     for o_item in overlapping_items[item_a]: # All items which could possibly be swappable with a
         if o_item not in pot_swaps_items: continue
@@ -243,7 +308,7 @@ def update_other_a_lists_with_b(item_a, item_b, pot_swaps, start_end_timestamps,
     return pot_swaps
 
 # For each item, store all items for which a swap would improve the pair's total rank error
-def init_pot_swaps(start_end_timestamps, op_type):
+def init_pot_swaps(start_end_timestamps, op_type, overlapping_items):
     pot_swaps = dict() # item:[(item to swap, rank error improvement)]
     for (item1, item1_list) in overlapping_items.items():
         pot_swaps_list = []
@@ -257,10 +322,10 @@ def init_pot_swaps(start_end_timestamps, op_type):
 # EXECUTE SWAPS
 # For each item, executes the best potential swap available
 # Ignores items that have been swapped this iteration
-def execute_and_update(pot_swaps, start_end_timestamps, op_type):
+def execute_and_update(pot_swaps, start_end_timestamps, op_type, overlapping_items):
     nr_swaps_this_iteration = 0
-    has_been_swapped = [] # TODO:Set?
-    for item1, item1_list in pot_swaps.items(): # TODO: Feels like we should need to update overlapping_items
+    has_been_swapped = []
+    for item1, item1_list in pot_swaps.items():
         if item1 in has_been_swapped: continue
         best_imp = (None, 0)
         for (item2, re_imp) in item1_list: 
@@ -274,10 +339,10 @@ def execute_and_update(pot_swaps, start_end_timestamps, op_type):
 
             # Update pot_swaps:
             # Swap the two item's potential swap lists, and account for their own occurence in the respective list
-            pot_swaps = update_a_with_bs_list(item1, best_imp[0], pot_swaps, start_end_timestamps, op_type)
-            pot_swaps = update_a_with_bs_list(best_imp[0], item1, pot_swaps, start_end_timestamps, op_type)
+            pot_swaps = update_a_with_bs_list(item1, best_imp[0], pot_swaps, start_end_timestamps, op_type, overlapping_items)
+            pot_swaps = update_a_with_bs_list(best_imp[0], item1, pot_swaps, start_end_timestamps, op_type, overlapping_items)
 
             # Update all other items' potential swap lists
-            pot_swaps = update_other_a_lists_with_b(item1, best_imp[0], pot_swaps, start_end_timestamps, op_type)
-            pot_swaps = update_other_a_lists_with_b(best_imp[0], item1, pot_swaps, start_end_timestamps, op_type)
+            pot_swaps = update_other_a_lists_with_b(item1, best_imp[0], pot_swaps, start_end_timestamps, op_type, overlapping_items)
+            pot_swaps = update_other_a_lists_with_b(best_imp[0], item1, pot_swaps, start_end_timestamps, op_type, overlapping_items)
     return (pot_swaps, nr_swaps_this_iteration)
